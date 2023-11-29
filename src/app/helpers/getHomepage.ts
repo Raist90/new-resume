eea36683f3ef0bc89cefe8da517cfaa597dae20f
Jaromil90 <manuel.detoma@noaignite.com>
2023-11-29 18:34:04 +0100

    parse getHomepage incoming data with Zod

--- src/app/helpers/getHomepage.ts
+++ src/app/helpers/getHomepage.ts
@@ -1,11 +1,11 @@
 /** @todo Fix this ugly import */
 import { client } from '../../../sanity/lib/client'
 import { homepageQuery } from '../api/sanity'
-import { HomepageProps } from '../types'
+import { HomepageProps, homepageSchema } from '@/app/types'
 
 export const getHomepage = async () => {
   const sanityClient = client
-  const homepage: Awaited<HomepageProps> =
-    await sanityClient.fetch(homepageQuery)
+  const data: Awaited<HomepageProps> = await sanityClient.fetch(homepageQuery)
+  const homepage = homepageSchema.parse(data)
   return homepage
 }
--- src/app/types/pages.ts
+++ src/app/types/pages.ts
@@ -1,15 +1,33 @@
-import type { TypedObject } from 'sanity'
+import z from 'zod'
 
-export type HomepageProps = {
-  title: string
-  profile: {
-    name: string
-    motto: string
-    picture: {
-      alt: string
-      src: string
-    }
-    role: string
-    bio: TypedObject[]
-  }
-}
+/** @description This data is coming from Sanity CMS */
+export const homepageSchema = z.object({
+  title: z.string(),
+  profile: z.object({
+    name: z.string(),
+    motto: z.string(),
+    picture: z.object({
+      alt: z.string(),
+      src: z.string().url(),
+    }),
+    role: z.string(),
+    bio: z.array(
+      z.object({
+        _key: z.string(),
+        _type: z.string(),
+        children: z.array(
+          z.object({
+            _key: z.string(),
+            _type: z.string(),
+            marks: z.array(z.string()),
+            text: z.string(),
+          }),
+        ),
+        markDefs: z.array(z.unknown()),
+        style: z.string(),
+      }),
+    ),
+  }),
+})
+
+export type HomepageProps = z.infer<typeof homepageSchema>
