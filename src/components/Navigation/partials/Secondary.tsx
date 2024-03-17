812f1064b7a0978521087dfc0ef6f6078d672911
Jaromil90 <manuel.detoma@noaignite.com>
2024-03-17 16:45:29 +0100

    fix Secondary by adding z-50

--- src/components/Navigation/partials/Secondary.tsx
+++ src/components/Navigation/partials/Secondary.tsx
@@ -2,7 +2,7 @@ import { ThemeSwitcher } from '@/components'
 
 export const Secondary = () => {
   return (
-    <ul role='menu'>
+    <ul role='menu' className='z-50'>
       <ThemeSwitcher />
     </ul>
   )
